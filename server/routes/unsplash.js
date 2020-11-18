e8a405feb8492d7f4c6911f57c7390a98f53a0a7
Alfred Adeoye <alfred.adeoye@cantab.net>
2020-11-18 20:19:17 +0000

    v0.2 user authentiaction and api routes

---
+++ server/routes/unsplash.js
@@ -0,0 +1,13 @@
+const express = require(express)
+const router = express.Router()
+
+router.get("/unsplash", async (req, res) => {
+      const color = req.body.color
+      const theme = req.body.theme
+
+      const results = await fetch(`api.unsplash.com/photos/random/?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=${theme}&color=${color}`)
+
+      const data = await results.json()
+
+      console.log(data)
+})
